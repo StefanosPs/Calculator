22bbf7f75e9d34ec8d479d93bae823414855f1d9
Stefanos Psarras <stepsarras@gmail.com>
2020-03-27 16:42:55 +0200

    Testers first commit

---
+++ src/components/calculator/operation-key/operation-key.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import {
+    mathOperationObj
+  } from "../../../classes/calculator/calculatorClass";
+
+import OperationKey from './operation-key';
+
+it('renders mathOperationObj when there are no props', () => {
+  const tree = renderer.create(<OperationKey />).toJSON();
+  expect(tree).toMatchSnapshot(); 
+});
+
+it('renders mathOperationObj when there are props', () => {
+    const numberKeyFull = renderer.create(<OperationKey mathOperationObj={mathOperationObj} />).toJSON();
+    expect(numberKeyFull).toMatchSnapshot();
+});
